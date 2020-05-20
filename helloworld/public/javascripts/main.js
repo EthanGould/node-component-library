6727f5e44128534a30bc12a84e89c1d9d3373ad5
Ethan Gould <ethan.g.907@gmail.com>
2020-05-20 15:43:27 -0400

    brand themeing with css variables

--- helloworld/public/javascripts/main.js
+++ helloworld/public/javascripts/main.js
@@ -3,11 +3,14 @@ let app = {};
 
 app.init = () => {
 	els.copy = document.querySelectorAll('.js-copy');
+	els.brandSelect = document.querySelector('.js-brand-select');
 	app.eventsHandlers();
+	app.setBrand();
 }
 
 app.eventsHandlers = () => {
 	els.copy.forEach((el) => { el.addEventListener('click', app.copyToClipboard) });
+	els.brandSelect.addEventListener('change', app.updateBrand);
 }
 
 // This doesnt work tho..
@@ -16,6 +19,15 @@ app.copyToClipboard = (e) => {
 	input.value = e.target.dataset.value;
 	input.select();
 	document.execCommand('copy');
+}
+
+app.updateBrand = (e) => {
+	window.location.search = `brand=${e.target.value}`;
+}
+
+app.setBrand = () => {
+	els.brandSelect.value = els.brandSelect.dataset.selection || 'as';
+	document.body.dataset.brand = els.brandSelect.value;
 }
 
 app.init();
--- helloworld/routes/patterns.js
+++ helloworld/routes/patterns.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET patterns listing. */
 router.get('/:pattern', function(req, res) {
-	res.render(req.params.pattern);
+	res.render(req.params.pattern, { brand: req.query.brand });
 });
 
 module.exports = router;
