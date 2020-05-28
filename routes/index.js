43b190b8d9ca1f8c05ab3f63eda973ef3e7063f5
Ethan Gould <ethan.g.907@gmail.com>
2020-05-28 13:53:11 -0400

    adding logo and title tag

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ethann' });
+  res.render('index');
 });
 
 module.exports = router;
